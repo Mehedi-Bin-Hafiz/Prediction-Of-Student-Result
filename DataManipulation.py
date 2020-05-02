2e68f088a469bf62d5a1e5dd3950fcedaeafc600
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-05-03 01:15:57 +0600

    Data Manipulation by pandas

--- DataManipulation.py
+++ DataManipulation.py
@@ -1,59 +1,88 @@
 import pandas as pd
 import numpy as np
-maindata=pd.read_excel(r'Main_data.xls',index=False).fillna(0)
-
-M1=maindata.iloc[:, 3:-2].values
-
-mid1=[]
-for i in range(0,len(M1)):
-    mid1.append(M1.item(i))
-M2=maindata.iloc[:, 4:-1].values
-mid2=[]
-for i in range(0,len(M2)):
-    mid2.append(M2.item(i))
-
-Mid=[]
-for i in range(len(mid1)):
-     if(mid1[i]>mid2[i]):
-         Mid.append(mid1[i])
-     else:
-         Mid.append(mid2[i])
 
 
 
-GPA=maindata.iloc[:, 5: ].values
-TotalMark=[]
-for i in range(0,len(GPA)):
-    TotalMark.append(GPA.item(i)/0.05)
-
-Quiz=((maindata[['Q1','Q2','Q3']].astype(float).sum(1))/3).apply(np.ceil)
-maindata.insert(3,"Quiz",Quiz,True)
-maindata.insert(6,"Mid",Mid,True)
-maindata.insert(7,"TotalMark",TotalMark,True)
-finaldata=maindata[['Quiz','Mid','CGPA','TotalMark']]
-
-# finaldata.to_excel(r'QuizMidSgpa.xlsx', index = False)
-latestdata=finaldata.loc[finaldata['Quiz'] != 0 ]
-latestdata2=latestdata.loc[latestdata['Mid'] != 0]
-
-
-print(latestdata2)
-# latestdata.to_excel(r'Path where you want to store the exported excel file\File Name.xlsx', index = False)
-
-"""QIZ MID GPA and TotalMark found"""
+""""   Very Dangerous program. Never run it without permission  """""
 
 
 
-T=latestdata2.iloc[:, 3: ].values
-tm=T.item(0)
-p=tm/100
-at=7*p
-print(at)
-pr=8*p
-print(pr)
-ass=5*p
-print(ass)
-fin=tm-(at+pr+ass+9+18.80)
-print(fin)
-tota=at+pr+ass+fin+9+18.80
-print(tota)
+
+
+
+# maindata=pd.read_excel(r'Main_data.xls',index=False).fillna(0)
+#
+# M1=maindata.iloc[:, 3:-2].values
+#
+# mid1=[]
+# for i in range(0,len(M1)):
+#     mid1.append(M1.item(i))
+# M2=maindata.iloc[:, 4:-1].values
+# mid2=[]
+# for i in range(0,len(M2)):
+#     mid2.append(M2.item(i))
+#
+# Mid=[]
+# for i in range(len(mid1)):
+#      if(mid1[i]>mid2[i]):
+#          Mid.append(mid1[i])
+#      else:
+#          Mid.append(mid2[i])
+#
+# #############modifying quiz and mid mark###############
+# Quiz=((maindata[['Q1','Q2','Q3']].astype(float).sum(1))/3).apply(np.ceil)
+# maindata.insert(3,"Quiz",Quiz,True)
+# maindata.insert(6,"Mid",Mid,True)
+# ############# modifying quiz and mid mark ###############
+#
+# # print(maindata)
+#
+# ################# Generating attendence presentation assingment ###########
+# GPA=maindata.iloc[:, 7: ].values
+# TotalMark=[]
+# for i in range(0,len(GPA)):
+#     TotalMark.append(GPA.item(i)/0.05)
+# # print(TotalMark)
+# attendance=[]
+# presentation=[]
+# assignment=[]
+# for i in TotalMark:
+#     tm = i
+#     p = tm / 100
+#     attendance.append(7 * p)
+#     presentation.append(8 * p)
+#     assignment.append(5*p)
+# maindata.insert(1,"Attendance",attendance,True)
+# maindata.insert(2,'Presentation',presentation,True)
+# maindata.insert(4,'Assignment',assignment,True)
+# maindata.insert(5,'TotalMark',TotalMark,True)
+# #################Generating attendence presentation assingment ###########
+#
+#
+#
+# finaldata=maindata[['Quiz',"Attendance",'Presentation','Assignment','Mid','TotalMark','CGPA']].round(decimals=2)
+#
+# # finaldata.to_excel(r'QuizMidSgpa.xlsx', index = False)
+# latestdata=finaldata.loc[finaldata['Quiz'] != 0 ]
+# latestdata2=latestdata.loc[latestdata['Mid'] != 0]
+#
+# print(latestdata2)
+#
+# #####latestdata2.to_excel(r'WithoutFinalData.xlsx', index = False) #should not run with out permission
+#
+
+# """ QIZ MID assignment presentation attendance GPA and TotalMark found """
+#
+#
+# WFdatabase=pd.read_excel(r'WithoutFinalData.xlsx',index=False)
+#
+# Sumdata=WFdatabase[['Quiz',"Attendance",'Presentation','Assignment','Mid']].astype(float).sum(1)
+#
+# FinalMark= WFdatabase["TotalMark"]-Sumdata
+# WFdatabase.insert(5,"Final",FinalMark,True)
+# WFdatabase.to_excel(r'ManipulatedData.xlsx', index = False)
+
+
+
+
+
