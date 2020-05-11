fcb0367363bf09f98e18b57da62e39c0c0d2d65c
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-05-11 23:55:20 +0600

    see practice

--- for.py
+++ for.py
@@ -2,3 +2,7 @@ lis=[]
 for i in range(0,46,5):
     lis.append(i)
 print(lis)
+import pandas as pd
+import numpy as np
+df2 = pd.DataFrame(np.random.rand(10, 4), columns=['a', 'b', 'c', 'd'])
+print(df2)
